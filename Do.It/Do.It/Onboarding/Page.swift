da1ed55c9eec70d2028ae4166769b9d28ee86c8b
karolyneehcs <anakarolina1966@gmail.com>
2020-08-17 11:16:51 -0300

    adicao de Struct

---
+++ Do.It/Do.It/Onboarding/Page.swift
@@ -0,0 +1,9 @@
+//
+//  Page.swift
+//  Do.It
+//
+//  Created by Ana Karolina Costa da Silva on 17/08/20.
+//  Copyright © 2020 Ana Karolina Costa da Silva. All rights reserved.
+//
+
+import Foundation
