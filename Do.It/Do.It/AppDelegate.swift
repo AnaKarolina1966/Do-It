383dac92230e355f89395f4082502cc040b677de
karolyneehcs <anakarolina1966@gmail.com>
2020-08-24 20:25:31 -0300

    testes de interface

--- Do.It/Do.It/AppDelegate.swift
+++ Do.It/Do.It/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        configureForTests()
         return true
     }
 
@@ -28,6 +28,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    private func configureForTests() {
+        if CommandLine.arguments.contains("-reset") {
+            let defaultsName = Bundle.main.bundleIdentifier!
+            UserDefaults.standard.removePersistentDomain(forName: defaultsName)
+        }
     }
 
 }
